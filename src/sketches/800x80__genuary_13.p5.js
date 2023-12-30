672331f40fa15b3508ff9199c525fd7a1ecfee89
Adam Bachman <adam.bachman@gmail.com>
2023-12-30 00:19:11 -0500

    more renames

---
+++ src/sketches/800x80__genuary_13.p5.js
@@ -0,0 +1,140 @@
+const STEP = 8,
+  W = 800,
+  H = 80
+const ROWS = H / STEP,
+  COLS = W / STEP
+
+let World = []
+
+for (let y = 0; y < H / STEP; y++) {
+  const r = []
+  for (let x = 0; x < W / STEP; x++) {
+    r.push(false)
+  }
+  World.push(r)
+}
+
+function setup() {
+  createCanvas(W, H)
+  noStroke()
+  // enableCapture({
+  //   frameCount: 300,
+  //   frameRate: 30,
+  // })
+}
+
+const MODELS = {
+  glider: [
+    [0, 0],
+    [1, 0],
+    [2, 0],
+    [0, 1],
+    [1, 2],
+  ],
+  r: [
+    [0, 1],
+    [1, 0],
+    [1, 1],
+    [1, 2],
+    [2, 0],
+  ],
+}
+
+// matrix moves
+const Model = {
+  transpose(model) {
+    return model.map((coord) => coord.reverse())
+  },
+
+  flip(model) {
+    // mirror over horizontal axis
+    const ymax = model.reduce((p, c) => (p > c[1] ? p : c[1]), 0)
+    return model.map((coord) => [coord[0], int(map(coord[1], 0, ymax, ymax, 0))])
+  },
+
+  translate(model, cx, cy) {
+    return model.map((coord) => [coord[0] + cx, coord[1] + cy])
+  },
+
+  rotate(model, rot) {
+    // left 90deg rotation
+    let next = model.slice(0)
+    for (let n = 0; n < rot; n++) {
+      next = Model.flip(Model.transpose(next))
+    }
+    return next
+  },
+}
+
+// drop a random glider onto the world
+function launch() {
+  const model = "glider"
+  const next = Model.translate(
+    Model.rotate(MODELS[model], floor(random(0, 4))),
+    floor(random(COLS - 2)),
+    floor(random(ROWS - 2)),
+  )
+  next.forEach((coord) => (World[coord[1]][coord[0]] = true))
+}
+
+const UNIT_SQUARE = [
+  [-1, -1],
+  [0, -1],
+  [1, -1],
+  [-1, 0],
+  [1, 0],
+  [-1, 1],
+  [0, 1],
+  [1, 1],
+]
+
+function neighbors(x, y, cb) {
+  return UNIT_SQUARE.map((p) => [(COLS + p[0] + x) % COLS, (ROWS + p[1] + y) % ROWS])
+}
+
+function draw() {
+  // background(220);
+  fill(80, 0, 0, 20)
+  rect(0, 0, width, height)
+
+  // update
+  const next = []
+  for (let y = 0; y < ROWS; y++) {
+    const r = []
+    for (let x = 0; x < COLS; x++) {
+      let self = World[y][x]
+      let alive = 0
+      const ns = neighbors(x, y)
+      for (let n = 0; n < ns.length; n++) {
+        alive += World[ns[n][1]][ns[n][0]] ? 1 : 0
+      }
+
+      if (alive == 3) {
+        r.push(true)
+      } else if (self && alive == 2) {
+        r.push(true)
+      } else {
+        r.push(false)
+      }
+    }
+    next.push(r)
+  }
+  World = next
+
+  // display
+  fill(100, 200, 240)
+  for (let y = 0; y < ROWS; y++) {
+    const r = []
+    for (let x = 0; x < COLS; x++) {
+      if (World[y][x]) {
+        rect(x * STEP, y * STEP, STEP)
+      }
+    }
+  }
+
+  if (frameCount % 20 == 0) {
+    launch("glider")
+  }
+
+  // captureFrame()
+}
---
+++ src/sketches/raycasting_2d.p5.js
@@ -0,0 +1,92 @@
+//2D visibility
+// Ray Casting
+
+import { Ray } from "./raycasting/Ray.js";
+import { Boundary } from "./raycasting/Boundary.js";
+import p5 from "p5";
+
+const CAPTURE = false;
+
+const source = { pos: null, rays: [] };
+const walls = [];
+let xoff;
+let cf;
+const rad = 160;
+let t = 0;
+
+function setup() {
+  createCanvas(400, 400);
+
+  if (CAPTURE) {
+    enableCapture({
+      frameCount: 60,
+      element: document.getElementById("defaultCanvas0"),
+      onComplete: () => noLoop(),
+    });
+  }
+
+  xoff = width / 2 + rad * cos(radians(t));
+  yoff = height / 2 + rad * sin(radians(t));
+
+  source.pos = createVector(width / 2, height / 2);
+  for (let i = 0; i < 360; i += 1) {
+    source.rays.push(new Ray(source.pos, radians(i)));
+  }
+
+  for (let n = 0; n < 5; n++) {
+    const x1 = random(width);
+    const x2 = random(width);
+    const y1 = random(height);
+    const y2 = random(height);
+
+    walls.push(new Boundary(x1, y1, x2, y2));
+  }
+  walls.push(new Boundary(0, 0, width, 0)); // top
+  walls.push(new Boundary(0, 0, 0, height)); // left
+  walls.push(new Boundary(width, 0, width, height)); // right
+  walls.push(new Boundary(width, height, 0, height)); // bottom
+};
+
+function draw() {
+  background(0);
+
+  for (let wall of walls) {
+    wall.show();
+  }
+
+  ellipse(source.pos.x, source.pos.y, 10);
+  move(source);
+
+  for (let ray of source.rays) {
+    let closest;
+    let record = Infinity;
+    for (let wall of walls) {
+      let pt = ray.cast(wall);
+      if (pt) {
+        const d = p5.Vector.dist(ray.pos, pt);
+        if (d < record) {
+          record = d;
+          closest = pt;
+        }
+      }
+    }
+
+    if (closest) {
+      stroke(255, 100);
+      line(ray.pos.x, ray.pos.y, closest.x, closest.y);
+    }
+  }
+
+  // captureFrame();
+};
+
+function move(source) {
+  source.pos.set(xoff, yoff);
+  t += 1;
+
+  xoff = width / 2 + rad * cos(radians(t));
+  yoff = height / 2 + rad * sin(radians(t));
+  // source.pos.set(noise(xoff) * width, noise(yoff) * height)
+  // xoff += 0.005
+  // yoff += 0.005
+}
